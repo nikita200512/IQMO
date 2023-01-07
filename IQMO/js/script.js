a3aba46fc0ac4d001c16864ebb8c615ffdd5f4bc
nikita200512 <93154095+nikita200512@users.noreply.github.com>
2023-01-07 20:33:24 +0300

    Add files via upload

---
+++ IQMO/js/script.js
@@ -0,0 +1,5 @@
+$(function () {
+  $('.program__item-btn__1').on('click', function () {
+    $('.program__item-btn__1, .program__burger, .program__item-1, .program__item-img__1, .program__item-img__2').toggleClass('active')
+  });
+});
